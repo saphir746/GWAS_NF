358be28f47183f476e4873b8af64cccb9fd14e4b
saphir746 <deborah.schneider-luftman@crick.ac.uk>
2021-04-14 18:09:14 +0100

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   .gitignore
    	new file:   .run_GWAS_NF.sh.swp
    	new file:   Regenie_GWAS_NF.nf
    	new file:   nextflow.config
    	new file:   run_GWAS_NF.sh

---
+++ run_GWAS_NF.sh
@@ -0,0 +1,9 @@
+## Load modules
+ml purge
+ml nextflow/21.03.0-edge
+ml Singularity/3.4.2
+
+WORDIR=/camp/stp/babs/scratch/schneid/Regenie_GWA_NF/
+
+
+nextflow Regenie_GWAS_NF.nf --input -work-dir  -resume
